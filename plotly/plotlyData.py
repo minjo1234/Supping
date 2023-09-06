411d05fade967c5e7391c64bc80663be527e7704
minjo1234 <whals777530@naver.com>
2023-09-06 14:06:30 +0900

    14:06

---
+++ plotly/plotlyData.py
@@ -0,0 +1,11 @@
+# Plotly 제공 데이터셋
+# plotly에서는 express 를 통해 데이터셋을 불러올수 있습니다.
+
+# express 패키지 불러오기
+import plotly.express as px
+
+# 선거 데이터 불러오기
+election = px.data.election()
+election.head()
+# head를 이용해서 위에서 5개 가져오기 .
+print(election.head())
